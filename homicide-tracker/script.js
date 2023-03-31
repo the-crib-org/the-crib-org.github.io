47179f04b7bdf6f27ce28092d3dfef12c93d8186
z66n <47192580+z66n@users.noreply.github.com>
2023-03-31 19:50:41 -0400

    Fixed data source hyperlinks

--- homicide-tracker/script.js
+++ homicide-tracker/script.js
@@ -1,4 +1,5 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoibHVjaWZlcnllbGxvdyIsImEiOiJja2V1bWJkd2MxcHozMnltcmVzajE0NWs3In0.LjpvVCLIKvcCpSLqE4P-5Q';
+
+	  mapboxgl.accessToken = 'pk.eyJ1IjoibHVjaWZlcnllbGxvdyIsImEiOiJja2V1bWJkd2MxcHozMnltcmVzajE0NWs3In0.LjpvVCLIKvcCpSLqE4P-5Q';
     var map = new mapboxgl.Map({
         container: 'map',
         style: 'mapbox://styles/luciferyellow/ckf1ke60u5o3418p7n03byd0u',
@@ -182,9 +183,9 @@ mapboxgl.accessToken = 'pk.eyJ1IjoibHVjaWZlcnllbGxvdyIsImEiOiJja2V1bWJkd2MxcHozM
       
       
         if (nbhs.length > 0) {
-          document.getElementById('pd').innerHTML = '<h3><strong>' + nbhs[0].properties.AREA_NAME + '</strong></h3><p><strong><em>' + nbhs[0].properties.BLK + '</strong>% Black population. Median: 5.69%</em></p>' + '</strong></h3><p><strong><em>' + nbhs[0].properties.UNEMP + '</strong>% Unemployment rate. Median: 8.2%</em></p>' + '</strong></h3><p><strong><em>' + nbhs[0].properties.LOW_INCOME + '</strong>% Low income prevalence (LIM-AT). Median: 18.6%</em></p>' + '</strong></h3><p><strong><em>' + nbhs[0].properties.NO_CERT + '</strong>% No certificate, diploma or degree. Median: 16.42%</em></p>' + '</strong></h3><p><strong><em>' + '<small>Source: <a href="https://open.toronto.ca/dataset/neighbourhood-profiles/">2016 Census Neighbourhood Profiles</a></small></em>'; 
+          document.getElementById('pd').innerHTML = '<h3><strong>' + nbhs[0].properties.AREA_NAME + '</strong></h3><p><strong><em>' + nbhs[0].properties.BLK + '</strong>% Black population. Median: 5.69%</em></p>' + '</strong></h3><p><strong><em>' + nbhs[0].properties.UNEMP + '</strong>% Unemployment rate. Median: 8.2%</em></p>' + '</strong></h3><p><strong><em>' + nbhs[0].properties.LOW_INCOME + '</strong>% Low income prevalence (LIM-AT). Median: 18.6%</em></p>' + '</strong></h3><p><strong><em>' + nbhs[0].properties.NO_CERT + '</strong>% No certificate, diploma or degree. Median: 16.42%</em></p>' + '</strong></h3><p><small><em>' + 'Source: <a href="https://open.toronto.ca/dataset/neighbourhood-profiles/">2016 Census Neighbourhood Profiles</small></em></a></p>'; 
         } else {
-          document.getElementById('pd').innerHTML = '<p>Hover over a neighbourhood!</p>';
+          document.getElementById('pd').innerHTML = '<p>Hover over a neighbourhood!</p>' + '<br><br><br><br><br><br><br><p><small><em>' + 'Source: <a href="https://open.toronto.ca/dataset/neighbourhood-profiles/">2016 Census Neighbourhood Profiles</small></em></a></p>';
         }       
       });
       
